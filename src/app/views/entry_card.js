ed15ac2b093902eb153a59e254693aac7616bbfd
Charles Ellis <hamled@hamled.name>
2016-11-03 09:30:55 -0700

    Entry card has an edit button

--- src/app/views/entry_card.js
+++ src/app/views/entry_card.js
@@ -1,7 +1,11 @@
+import _ from 'underscore';
 import Backbone from 'backbone';
 
 const EntryCard = Backbone.View.extend({
   initialize: function() {
+    // Static components
+    this.editBtn = Backbone.$('<button class="btn-edit">Edit</button>');
+
     this.listenTo(this.model, 'change', this.render);
     this.render();
   },
@@ -9,12 +13,21 @@ const EntryCard = Backbone.View.extend({
   // RENDERING
   render: function() {
     this.$('*').detach();
-    this.$el.append(this.renderName(this.model.get('name')));
+    this.$el.append(_.flatten([
+      this.renderName(this.model.get('name')),
+      this.renderControls()
+    ]));
     return this;
   },
 
   renderName: function(name) {
     return Backbone.$('<p></p>').html(name);
+  },
+
+  renderControls: function() {
+    return [
+      this.editBtn
+    ];
   }
 });
 
