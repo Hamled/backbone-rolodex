1a8351cdb8b8ad97cfa330a869c05914431f956c
Charles Ellis <hamled@hamled.name>
2016-11-03 07:12:25 -0700

    Rolodex view

---
+++ src/app/views/rolodex_view.js
@@ -0,0 +1,21 @@
+import Backbone from 'backbone';
+import EntryCard from './entry_card';
+
+const RolodexView = Backbone.View.extend({
+  initialize: function() {
+    this.listenTo(this.model, 'change', this.render);
+    this.render();
+  },
+
+  // RENDERING
+  render: function() {
+    this.$el.append(this.model.get('entries').map(function(entry) {
+      return new EntryCard({
+        model: entry
+      }).$el;
+    }));
+    return this;
+  }
+});
+
+export default RolodexView;
