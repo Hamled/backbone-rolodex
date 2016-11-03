bf31d9bc9ebd4566cbd98e607ab37917bcaba6f7
Charles Ellis <hamled@hamled.name>
2016-11-03 06:43:46 -0700

    Rolodex Entry model

---
+++ src/app/models/entry.js
@@ -0,0 +1,17 @@
+import Backbone from 'backbone';
+
+// A single rolodex entry
+const Entry = Backbone.Model.extend({
+  // Attributes:
+  //   name
+  //   organization
+  //   phone
+  //   address
+  //     street1
+  //     street2
+  //     city
+  //     state
+  //     postal
+});
+
+export default Entry;
