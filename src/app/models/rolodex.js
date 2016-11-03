4b58be70b9e4dc81a769b68b7d50a70e47dcebe9
Charles Ellis <hamled@hamled.name>
2016-11-03 08:54:13 -0700

    Rolodex triggers change when entries list changes

--- src/app/models/rolodex.js
+++ src/app/models/rolodex.js
@@ -4,10 +4,16 @@ import {Entry, Entries} from './entry';
 const Rolodex = Backbone.Model.extend({
   initialize: function() {
     this.set('entries', new Entries());
+    this.listenTo(this.get('entries'), 'update', this.onEntriesUpdate);
   },
 
   addEntry: function(entryData) {
     this.get('entries').add(new Entry(entryData));
+  },
+
+  // EVENTS
+  onEntriesUpdate: function() {
+    this.trigger('change', arguments);
   }
 });
 
