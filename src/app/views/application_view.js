66fa3460f74d1e735921ad6a651a0676b157610e
Charles Ellis <hamled@hamled.name>
2016-11-03 08:55:07 -0700

    Application view has entry form

--- src/app/views/application_view.js
+++ src/app/views/application_view.js
@@ -1,17 +1,33 @@
 import Backbone from 'backbone';
+import EntryForm from './entry_form';
 import RolodexView from './rolodex_view';
 
 const ApplicationView = Backbone.View.extend({
   initialize: function() {
+    // Static components
+    this.entryForm = new EntryForm({
+      parent: this
+    });
+
     this.listenTo(this.model, 'change', this.render);
     this.render();
+  },
+
+  // EVENTS
+  onAddEntry: function(entryData) {
+    this.model.rolodex().addEntry(entryData);
   },
 
   // RENDERING
   render: function() {
     this.$('*').detach();
+    this.renderStatic();
     this.$el.append(this.renderRolodex(this.model.rolodex()));
     return this;
+  },
+
+  renderStatic: function() {
+    this.$el.append(this.entryForm.el);
   },
 
   renderRolodex: function(rolodex) {
