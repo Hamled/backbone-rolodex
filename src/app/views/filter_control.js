0757ab2150b4e3bf5802692caab8c8080806e04f
Charles Ellis <hamled@hamled.name>
2016-11-06 15:36:50 -0800

    Filter control view

---
+++ src/app/views/filter_control.js
@@ -0,0 +1,37 @@
+import Backbone from 'backbone';
+
+const FilterControl = Backbone.View.extend({
+  initialize: function() {
+    this.render();
+  },
+
+  // EVENTS
+  events: {
+    'input .filter-input': 'onChange',
+    'click .filter-clear-btn': 'onClear',
+  },
+
+  onChange: function(e) {
+    this.trigger('submit', this.$('.filter-input').val());
+  },
+
+  onClear: function(e) {
+    this.$('.filter-input').val('');
+    this.trigger('submit', '');
+  },
+
+  // RENDERING
+  render: function() {
+    this.$el.append(Backbone.$(`
+      <label>
+        Filter:
+        <input type="text" class="filter-input">
+      </label>
+      <button class="filter-clear-btn">Clear</button>
+    `));
+
+    return this;
+  }
+});
+
+export default FilterControl;
