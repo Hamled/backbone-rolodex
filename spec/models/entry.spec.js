63b2e70767ab9dde915384b9bb68c74610df5034
Charles Ellis <hamled@hamled.name>
2016-11-09 17:22:51 -0800

    Specs for Entry's filter predicate function

--- spec/models/entry.spec.js
+++ spec/models/entry.spec.js
@@ -1,9 +1,41 @@
+import _ from 'underscore';
 import Entry from 'app/models/entry';
+import fixtures from 'spec/fixtures';
 
 describe('Entry', function() {
   describe('.filterPred', function() {
     it('should return a function', function() {
       expect(Entry.filterPred('')).toBeFunction();
+    });
+
+    it('should filter on name attribute', function() {
+      var one = fixtures.entry().one;
+
+      var filterPred = Entry.filterPred('Person One');
+      expect(_.filter([one], filterPred)).toContain(one);
+
+      filterPred = Entry.filterPred('Person Two');
+      expect(_.filter([one], filterPred)).not.toContain(one);
+    });
+
+    it('should filter on organization attribute', function() {
+      var one = fixtures.entry().one;
+
+      var filterPred = Entry.filterPred('Org One');
+      expect(_.filter([one], filterPred)).toContain(one);
+
+      filterPred = Entry.filterPred('Org Two');
+      expect(_.filter([one], filterPred)).not.toContain(one);
+    });
+
+    it('should filter on phone attribute', function() {
+      var one = fixtures.entry().one;
+
+      var filterPred = Entry.filterPred('Phone One');
+      expect(_.filter([one], filterPred)).toContain(one);
+
+      filterPred = Entry.filterPred('Phone Two');
+      expect(_.filter([one], filterPred)).not.toContain(one);
     });
   });
 });
