3a6e42ffd8f2e213e0d574969e5e6e3acc726c54
Charles Ellis <hamled@hamled.name>
2016-11-09 16:08:42 -0800

    Test for Entry's filter predicate function

--- test/models/entry.js
+++ test/models/entry.js
@@ -1,9 +1,21 @@
+import _ from 'underscore';
 import Entry from 'app/models/entry';
+import fixtures from 'test/fixtures';
 
 describe('Entry', function() {
   describe('.filterPred', function() {
     it('should return a function', function() {
       assert.typeOf(Entry.filterPred(''), 'function');
+    });
+
+    it('should filter on name attribute', function() {
+      var one = fixtures.entry().one;
+
+      var filterPred = Entry.filterPred('Person One');
+      assert.include(_.filter([one], filterPred), one);
+
+      filterPred = Entry.filterPred('Person Two');
+      assert.notInclude(_.filter([one], filterPred), one);
     });
   });
 });
